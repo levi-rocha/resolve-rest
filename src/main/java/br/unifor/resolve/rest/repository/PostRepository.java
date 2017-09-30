386b641a7107444c3b107abc56f1138f0a25c317
Levi Rocha <ilevidr@gmail.com>
2017-09-30 16:24:54 -0300

    post resource refatorado para facilitar sort de votes

--- src/main/java/br/unifor/resolve/rest/repository/PostRepository.java
+++ src/main/java/br/unifor/resolve/rest/repository/PostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @RepositoryRestResource(collectionResourceRel = "post", exported = false)
 public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
@@ -19,11 +20,14 @@ public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 
     Page<Post> findAll(Pageable pageable);
 
-    List<Post> findByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(
-            String title, String content);
-
     Post save(Post post);
 
-    Page<Post> findByAuthorId(Pageable pageable, Long authorId);
+    List<Post> findDistinctByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(
+            String content, String title);
+
+    Page<Post> findByIdIn(Pageable pageable, List<Long> ids);
+
+    List<Post> findDistinctByAuthorId(Long authorId);
+    Page<Post> findDistinctByAuthorId(Pageable pageable, Long authorId);
 
 }
